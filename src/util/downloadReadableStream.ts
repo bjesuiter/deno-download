e63df98610ffbb47af03e766016fe77b573dc73e
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-04-18 12:04:40 +0200

    add skeleton of downloadReadableStream.ts

---
+++ src/util/downloadReadableStream.ts
@@ -0,0 +1,7 @@
+/**
+ * A function for downloading a file and returning a readable stream
+ * allows to decouple downloading from further processing
+ * (writing to file, sending to another network stream, etc.)
+ */
+export function downloadReadableStream(input: string | Request) {
+}
